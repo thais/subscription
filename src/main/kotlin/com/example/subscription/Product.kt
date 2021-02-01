c500665c1e2b60fe6d58c639e35cfec8ccf952c1
Thais Hamilton <thaismha@gmail.com>
2021-02-01 21:17:17 -0200

    first commit

---
+++ src/main/kotlin/com/example/subscription/Product.kt
@@ -0,0 +1,13 @@
+package com.example.subscription
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity(name = "products")
+data class Product(
+        @Id @GeneratedValue
+        val id: Long? = null,
+        val name: String,
+        val price: Double
+)
