e4313756c8045ff0fcbfd1150279363211ddc579
Thais Hamilton <thaismha@gmail.com>
2021-02-03 21:04:11 -0200

    adds all product implementations

--- src/main/kotlin/com/example/subscription/controller/ProductController.kt
+++ src/main/kotlin/com/example/subscription/controller/ProductController.kt
@@ -1,7 +1,6 @@
 package com.example.subscription.controller
 
 import com.example.subscription.model.Product
-import com.example.subscription.repository.ProductRepository
 import com.example.subscription.service.ProductService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/products")
-class ProductController(private val productService: ProductService, private val repository: ProductRepository) {
+class ProductController(private val productService: ProductService) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -19,24 +18,19 @@ class ProductController(private val productService: ProductService, private val 
     fun getAll(): List<Product> = productService.findAll()
 
     fun getById(@PathVariable id: Long): ResponseEntity<Product> =
-            repository.findById(id).map {
+            productService.getById(id).map {
                 ResponseEntity.ok(it)
             }.orElse(ResponseEntity.notFound().build())
 
     @PutMapping("/{id}")
     fun update(@PathVariable id: Long, @RequestBody product: Product): ResponseEntity<Product> =
-            repository.findById(id).map {
-                val product = it.copy(
-                        name = it.name,
-                        price = it.price
-                )
-                ResponseEntity.ok(repository.save(product))
+            productService.update(id, product).map {
+                ResponseEntity.ok(it)
             }.orElse(ResponseEntity.notFound().build())
 
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: Long): ResponseEntity<Void> =
-            repository.findById(id).map {
-                repository.delete(it)
-                ResponseEntity<Void>(HttpStatus.OK)
-            }.orElse(ResponseEntity.notFound().build())
+    fun delete(@PathVariable id: Long): ResponseEntity<Void> {
+        productService.delete(id)
+        return ResponseEntity<Void>(HttpStatus.OK)
+    }
 }
--- src/main/kotlin/com/example/subscription/service/ProductService.kt
+++ src/main/kotlin/com/example/subscription/service/ProductService.kt
@@ -2,9 +2,16 @@ package com.example.subscription.service
 
 import com.example.subscription.model.Product
 import org.springframework.web.bind.annotation.RequestBody
+import java.util.*
 
 interface ProductService {
     fun create(@RequestBody product: Product): Product
 
     fun findAll() : List<Product>
+
+    fun getById(id: Long): Optional<Product>
+
+    fun update(id: Long, account: Product) : Optional<Product>
+
+    fun delete(id: Long)
 }
